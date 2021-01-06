ac0ae94bb501be5e88b1c4e03f6827344e43730f
Huy-Ngo <duchuy29092000@gmail.com>
2021-01-06 11:18:30 +0700

    Split project-info for reuse
    I also use p/project/info for consistency with other tabs.

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,5 +1,6 @@
 # Test project pages
 # Copyright (C) 2020  Nguyễn Gia Phong
+# Copyright (C) 2020  Ngô Ngọc Đức Huy
 #
 # This file is part of Acanban.
 #
@@ -16,38 +17,48 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Acanban.  If not, see <https://www.gnu.org/licenses/>.
 
+from http import HTTPStatus as Status
+
 from quart.testing import QuartClient
 
 PROJECT = '/p/be7a04b8-650f-41dc-912b-10d225baff29/'
+PROJECT_INFO = f'{PROJECT}info'
+PROJECT_EDIT = f'{PROJECT}edit'
 
 
 async def test_info_nonexist(student: QuartClient) -> None:
     """Test accessing a nonexistent project info page."""
-    response = await student.get('/p/this-project-does-not-exist/')
-    assert response.status_code == 404  # not found
+    response = await student.get('/p/this-project-does-not-exist/info')
+    assert response.status_code == Status.NOT_FOUND
+
+
+async def test_redirect(client: QuartClient) -> None:
+    """Test the redirect for the client."""
+    response = await client.get(PROJECT)
+    assert response.status_code == Status.PERMANENT_REDIRECT
 
 
 async def test_info_access_guest(client: QuartClient) -> None:
     """Test project info page access by a guest."""
-    response = await client.get(PROJECT)
-    assert response.status_code == 401  # unauthorized
+    response = await client.get(PROJECT_INFO)
+    assert response.status_code == Status.UNAUTHORIZED
 
 
 async def test_info_access_assistant(assistant: QuartClient) -> None:
     """Test project info page access by an assistant."""
-    response = await assistant.get(PROJECT)
-    assert response.status_code == 401  # unauthorized
+    response = await assistant.get(PROJECT_INFO)
+    assert response.status_code == Status.UNAUTHORIZED
 
 
 # This supervisor is not a member of PROJECT.
 async def test_info_access_nonmember(supervisor: QuartClient) -> None:
     """Test project info page access by a non-member user."""
-    response = await supervisor.get(PROJECT)
-    assert response.status_code == 401  # unauthorized
+    response = await supervisor.get(PROJECT_INFO)
+    assert response.status_code == Status.UNAUTHORIZED
 
 
 # This student is a member of PROJECT.
 async def test_info_access_member(student: QuartClient) -> None:
     """Test project info page access by a member."""
-    response = await student.get(PROJECT)
-    assert response.status_code == 200  # OK
+    response = await student.get(PROJECT_INFO)
+    assert response.status_code == Status.OK
