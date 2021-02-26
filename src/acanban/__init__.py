e6eabb116e72a4cd072ee9f6d1cbdb12f71fa75c
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-02-26 22:58:32 +0700

    Stop setting min-width
    It broke scaling down.

--- src/acanban/__init__.py
+++ src/acanban/__init__.py
@@ -46,7 +46,7 @@ from .user import blueprint as user
 
 __all__ = ['app']
 __doc__ = 'Academic Kanban'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # Nope, the negative operator is not a typo, see also tzset(3posix).
 TZ = tz(timedelta(seconds=-timezone))
